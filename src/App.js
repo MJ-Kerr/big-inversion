1316e00d6cc900747efcfcdb55598235e944b6f9
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-03 01:25:48 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from'react';
 import './App.css';
+
+
+function Card ({ firstName, lastName, age, hairColor }) {
+  return (
+    <div className="card">
+      <h2>{lastName}, {firstName}</h2>
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card firstName="Jane" lastName="Doe" age={45} hairColor="black" />
+      <Card firstName="John" lastName="Smith" age={88} hairColor="brown" />
+      <Card firstName="Millard" lastName="Fillmore" age={50} hairColor="brown" />
+      <Card firstName="Maria" lastName="Smith" age={62} hairColor="brown" />
     </div>
   );
 }
